ec90b0b838c6f56587eedbcc92065204bf6199cc
Sdoody <31548856+Sdoody@users.noreply.github.com>
2017-09-06 13:27:41 -0500

    Changed name for main to HelloWolrd_SeanDoody.cpp

---
+++ HelloWorld_SeanDoody.cpp
@@ -0,0 +1,11 @@
+
+//  HelloWorld_SeanDoody
+//  Created by Sean Doody on 9/6/17.
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+
+    std::cout << "Hello, World!\n";
+    return 0;
+}
