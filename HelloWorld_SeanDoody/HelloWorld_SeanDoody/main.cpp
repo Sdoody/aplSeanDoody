70091b4c313efb49b116ed154adc6895511e896b
Sdoody <31548856+Sdoody@users.noreply.github.com>
2017-09-06 12:13:37 -0500

    added HelloWorld_SeanDoody
    git push
    .cd

---
+++ HelloWorld_SeanDoody/HelloWorld_SeanDoody/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWorld_SeanDoody
+//
+//  Created by Sean Doody on 9/6/17.
+//  Copyright © 2017 Sean Doody. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
